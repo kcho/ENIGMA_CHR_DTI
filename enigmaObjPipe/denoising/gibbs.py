586fec23c78bd9bef8bc1698dde65c8ccd3f671a
kc244 <kevincho@bwh.harvard.edu>
2022-05-03 23:12:27 -0400

    feat: gibbs to use fsl unring

--- enigmaObjPipe/denoising/gibbs.py
+++ enigmaObjPipe/denoising/gibbs.py
@@ -7,7 +7,6 @@ class NoiseRemovalPipe(object):
         if force or not dwi_unring.is_file():
             print(f'Running gibbs ringing on {dwi}')
             dwi_unring.parent.mkdir(parents=True, exist_ok=True)
-            # command = f'{self.unring} {dwi} {dwi_unring}'
-            command = f'cp {dwi} {dwi_unring}'
+            command = f'{self.unring} {dwi} {dwi_unring}'
 
             self.run(command)
