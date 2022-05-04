d7958cac66f9ea2caee2074368f914f464b5d4a6
kc244 <kevincho@bwh.harvard.edu>
2022-05-03 23:03:55 -0400

    minor

--- objPipe/utils/paths.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-def read_objPipe_config(config_loc: str):
-    '''Return config dictionary from config_loc'''
-    config = configparser.ConfigParser()
-    config.read(config_loc)
-
-    return config
--- tests/test_enigma_chr_pipeline.py
+++ tests/test_enigma_chr_pipeline.py
@@ -7,14 +7,30 @@ TEST_DATA_ROOT = SCRIPT_ROOT / 'test_data'
 import sys
 sys.path.append(str(SCRIPT_ROOT))
 
-from pipeline.enigma_chr_pipeline import EnigmaChrStudy
+from enigmaObjPipe.enigma_chr_pipeline import EnigmaChrStudy
+from eddy_squeeze.eddy_squeeze_lib.eddy_files import EddyRun, EddyDirectories
+from eddy_squeeze.eddy_squeeze_lib.eddy_web import create_html
 
-def test_check_dicom_info():
+
+def test_enigma_chr_subject():
     project_root = TEST_DATA_ROOT
 
     enigmaChrStudy = EnigmaChrStudy(project_root)
     enigmaChrSubject = enigmaChrStudy.subject_classes[0]
     enigmaChrSubject.subject_pipeline()
+
     
+def test_enigma_chr_project():
+    project_root = TEST_DATA_ROOT
 
+    enigmaChrStudy = EnigmaChrStudy(project_root)
+    for subject in enigmaChrStudy.subject_classes:
+        subject.subject_pipeline()
 
+    enigmaChrStudy.tbss_all = '/Users/kc244/tbss/lib/tbss_all'
+    enigmaChrStudy.tbss_all_modalities = ['dti_FA']
+    enigmaChrStudy.tbss_all_modalities_str = ['FA']
+    enigmaChrStudy.run_tbss()
+    # eddy_prefix_list = [x. for x in enigmaChrStudy.subject_classes]
+    # eddyDirectories = EddyDirectories(eddy_prefix_list)
+    # print(eddyDirectories)
