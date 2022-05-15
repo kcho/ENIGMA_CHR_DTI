48a0b3fac81366b88aca58c66dbe726eb957a653
kc244 <kevincho@bwh.harvard.edu>
2022-05-15 15:29:23 -0400

    feat: comments added to test script

--- docker/test/02_run_docker_image.sh
+++ docker/test/02_run_docker_image.sh
@@ -1,11 +1,17 @@
-## download example data
-#mkdir ~/test_ENIGMA_CHR_DTI
-#cd ~/test_ENIGMA_CHR_DTI
-#wget https://github.com/kcho/ENIGMA_CHR_DTI/releases/download/example_dwi_data/test_dwi_data.zip
-#unzip test_dwi_data.zip
-#rm test_dwi_data.zip
-#mv ~/test_ENIGMA_CHR_DTI/test_dwi_data ~/test_ENIGMA_CHR_DTI/sourcedata
-#cp -r ~/test_ENIGMA_CHR_DTI/sourcedata/subject_01 ~/test_ENIGMA_CHR_DTI/sourcedata/subject_02
+# create test data target location
+test_data_dir=~/test_ENIGMA_CHR_DTI
+mkdir ${test_data_dir}
+cd ~/${test_data_dir}
+
+# download example data
+wget https://github.com/kcho/ENIGMA_CHR_DTI/releases/download/example_dwi_data/test_dwi_data.zip
+unzip test_dwi_data.zip
+rm test_dwi_data.zip
+mv ~/${test_data_dir}/test_dwi_data ~/${test_data_dir}/sourcedata
+
+# duplicate subject01 to subject02
+cp -r ~/${test_data_dir}/sourcedata/subject_01 \
+    ~/${test_data_dir}/sourcedata/subject_02
 
 # run docker image called 'enigma_chr_pipeline'
 # and mount /Users/kc244/ENIGMA_CHR_DTI/test_data2 to /data of the image
