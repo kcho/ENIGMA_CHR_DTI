78b05d3429ef8116fa6cbb0ba18177db97cef05b
kc244 <kevincho@bwh.harvard.edu>
2022-05-17 20:46:17 -0400

    feat: code to return error message and exit the code when there is no data

--- scripts/collect_outputs.py
+++ scripts/collect_outputs.py
@@ -8,29 +8,51 @@ import tempfile as tf
 
 
 def compress_outputs():
+    '''Collect container based TBSS outputs
+    TODO:
+        - clean up the code
+    '''
     test_root = Path('/data')
+
+    if not test_root.is_dir():
+        print('No data mounted at /data')
+        return
     
     derivatives_root = test_root / 'derivatives'
+    if not derivatives_root.is_dir():
+        print(f'No {derivatives_root}')
+        return
     eddy_qc_root = derivatives_root / 'eddy_qc'
     screenshots_root = derivatives_root / 'screenshots'
     tbss_root = derivatives_root / 'tbss'
     web_summary_root = derivatives_root / 'web_summary'
 
     with tf.TemporaryDirectory() as fp:
-        (Path(fp) / 'tbss' / 'stats').mkdir(parents=True)
-        for i in (tbss_root / 'stats').glob('*csv'):
-            shutil.copy(i, Path(fp) / 'tbss')
+        if not (tbss_root / 'stats').is_dir():
+            print('No TBSS')
+        else:
+            (Path(fp) / 'tbss' / 'stats').mkdir(parents=True)
+            for i in (tbss_root / 'stats').glob('*csv'):
+                shutil.copy(i, Path(fp) / 'tbss')
 
         dirs_to_compress = [eddy_qc_root,
                             screenshots_root,
                             web_summary_root]
 
         for i in dirs_to_compress:
-            shutil.copytree(i, Path(fp) / i.name)
+            if i.is_dir():
+                shutil.copytree(i, Path(fp) / i.name)
+            else:
+                print(f'No data under {i}')
 
-        shutil.copytree(tbss_root / 'snapshots',
-                        Path(fp) / 'tbss' / 'snapshots')
-        shutil.make_archive('/data/output_collection', 'zip', fp)
+        if not (tbss_root / 'snapshots').is_dir():
+            shutil.copytree(tbss_root / 'snapshots',
+                            Path(fp) / 'tbss' / 'snapshots')
+
+        if len(Path(fp).glob('*')) > 1:
+            shutil.make_archive('/data/output_collection', 'zip', fp)
+        else:
+            print('No data to collect')
 
 
 if __name__ == '__main__':
--- scripts/enigma_chr_pipeline.py
+++ scripts/enigma_chr_pipeline.py
@@ -5,4 +5,9 @@ from enigmaObjPipe.enigma_chr_pipeline import EnigmaChrStudy
 if __name__ == '__main__':
     project_root = '/data'
     enigmaChrStudy = EnigmaChrStudy(project_root)
-    enigmaChrStudy.project_pipeline()
+    if len(enigmaChrStudy.subjects) > 1:
+        enigmaChrStudy.project_pipeline()
+    else:
+        print('Please check if there are dicom directories under /data')
+        print('(the input to -v option in the docker run command could be '
+               'wrong)')
