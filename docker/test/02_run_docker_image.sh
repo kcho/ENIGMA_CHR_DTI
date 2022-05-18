242a1c43ac1763f601851dc86e30826718dbd41b
kc244 <kevincho@bwh.harvard.edu>
2022-05-17 23:00:17 -0400

    docs: test lines added

--- docker/test/02_run_docker_image.sh
+++ docker/test/02_run_docker_image.sh
@@ -20,6 +20,10 @@ cp -r ${test_data_dir}/sourcedata/subject_01 \
 
 # run docker image called 'enigma_chr_pipeline'
 # and mount /Users/kc244/ENIGMA_CHR_DTI/test_data2 to /data of the image
+#docker run -it \
+    #-v ${test_data_dir}:/data \
+    #kcho/enigma-chr-pipeline
+
 docker run -it \
     -v ${test_data_dir}:/data \
-    kcho/enigma-chr-pipeline
+    enigma-chr-pipeline
