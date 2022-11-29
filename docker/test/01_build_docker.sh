fae7c6db4a51b3b0a0a257efd89c100839a1f977
kc244 <kevincho@bwh.harvard.edu>
2022-11-29 19:42:54 +0900

    feat: docker build using cache

--- docker/test/01_build_docker.sh
+++ docker/test/01_build_docker.sh
@@ -1,5 +1,6 @@
 # build docker based on the Dockerfile in the path ..
 # without cache
 # and name the output docker image as enigma_chr_pipeline
-docker build --no-cache -t enigma_chr_pipeline ..
+#docker build --no-cache -t enigma_chr_pipeline ..
+docker build -t enigma_chr_pipeline ..
 
